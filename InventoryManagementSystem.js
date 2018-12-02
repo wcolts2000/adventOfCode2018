da75d1866ac9a564418feb738815e1c073f586c8
wcolts2000 <wcolts2000@gmail.com>
2018-12-02 10:09:48 -0800

    part one of day 2 done

---
+++ InventoryManagementSystem.js
@@ -0,0 +1,321 @@
+const inventory = [
+  "wlpiogsvdfecjdqmnxakudrhbz",
+  "wbpioesvyfecjuqmlxaktdrhbz",
+  "blviogavyfecjuqmnxaktdrhbz",
+  "wlpiogsvydecjuqmnipktdrhbz",
+  "wlwiogsvyfmcjuqmoxaktdrhbz",
+  "wlpiogsvphecjuqmnxaktdrzbz",
+  "wlpiogsvyfecjuqmnkakhdrkbz",
+  "wlpiogsvyfhcjuqmnxxktddhbz",
+  "wlpiogsvyfccfuqmnxgktdrhbz",
+  "wlpiogsvhmecjvqmnxaktdrhbz",
+  "wlpiogsvyfecjdqqnxaktdrhyz",
+  "wlpiogyvycecjaqmnxaktdrhbz",
+  "wlpiogsvyfecjfqmnxaktybhbz",
+  "wkpiogsvyfmcauqmnxaktdrhbz",
+  "wlmiolsvyfecjuqmnxaktdrhbn",
+  "wlpioksvyfecjuqmnxaktdrhgs",
+  "wlpiogsvyflcjuvmnxsktdrhbz",
+  "wgziogsvyfecjuqmnxaktdrhoz",
+  "whpingsvyeecjuqmnxaktdrhbz",
+  "wlpiogsvyfecjuqgnxaktdvhlz",
+  "wlpioasvtfecjuqmnxaktdahbz",
+  "wlpihgsvefeceuqmnxaktdrhbz",
+  "wlpiogsvyfecyuqwnxaktdghbz",
+  "wlpfsgsvyfhcjuqmnxaktdrhbz",
+  "wlpiogyvyfecjuqmnxalpdrhbz",
+  "wlpiogsvyfescsqmnxaktdrhbz",
+  "wluiogsvyfecytqmnxaktdrhbz",
+  "wltiorsvyfecjuqmoxaktdrhbz",
+  "wlmiogwvyfejjuqmnxaktdrhbz",
+  "wlpiogsvyfycjuumnxvktdrhbz",
+  "wlkiogsqyfecjqqmnxaktdrhbz",
+  "wlpiogsvyfecouqmnxaktfrubz",
+  "hupiogsvlfecjuqmnxaktdrhbz",
+  "wlpiogsvpxecjuqmnxaksdrhbz",
+  "wlpiogsvyfkcjfqmnxxktdrhbz",
+  "wlpiogsjyfecjuqnnxakthrhbz",
+  "wlpiogsvyfecjuqmnxaktddhdk",
+  "wlpipgsvyfecjuqmnhaktdrubz",
+  "wlpiogsoyfenjpqmnxaktdrhbz",
+  "wlpiogsvyfecnuqmnxaxtdrmbz",
+  "wlpiggsvyfjcjuumnxaktdrhbz",
+  "wlppogsvyfecjuqmnxautdhhbz",
+  "wlpiovbvyfecjuqmnxaktcrhbz",
+  "wlpiogsvyfecjoqmnxaktdrobu",
+  "wlpiohsvyfecjugmnxakthrhbz",
+  "wvpiogovyfecjuqmnxakwdrhbz",
+  "wlpiogsbyfecjuqmnxdktdrtbz",
+  "wlpnogsvyfecjuqmnxakykrhbz",
+  "wlpiogpvyfecjuqmnxvktdrhbs",
+  "wlpiogsvkvecjuqmnxadtdrhbz",
+  "wlkihgsvyfecjuqmnxlktdrhbz",
+  "wlpilgsvyfhcjuqmnxakudrhbz",
+  "wlpioksvysgcjuqmnxaktdrhbz",
+  "wlpiogsvyfecorqmoxaktdrhbz",
+  "wlpiogsvyfectzlmnxaktdrhbz",
+  "wlpiogsvywecjuqwnqaktdrhbz",
+  "wlpiowsvyfecjuqrnxaftdrhbz",
+  "wlpiogsuyfecjutmnxaktnrhbz",
+  "wepiowsvyfqcjuqmnxaktdrhbz",
+  "wlpirgssyfecjuqmvxaktdrhbz",
+  "wlyiogstyfecjuqmnxaktdrhbw",
+  "wlpiogseyfecauqmnxaktdjhbz",
+  "wlpioisvyfenjuqmnxakrdrhbz",
+  "wlpiogsvyfrgjfqmnxaktdrhbz",
+  "wlpionsvyfecjmqmjxaktdrhbz",
+  "alpiggsvyfecjuqmnxaktdrhkz",
+  "wlphngsvyfecjuqmnxfktdrhbz",
+  "wlpiogsvyferjuqmlxakttrhbz",
+  "wlniogsvefecjuqsnxaktdrhbz",
+  "wlpiogsvyfncjurmnxakudrhbz",
+  "wlpiogsvyfecjuqmnxaktprlaz",
+  "wlpiocsvyfecjupmkxaktdrhbz",
+  "wlpihgsvyfecjeqfnxaktdrhbz",
+  "wlwioasvyfjcjuqmnxaktdrhbz",
+  "wlpifgsvyfecjuqsnxaktdshbz",
+  "wlxiogsvyrechuqmnxaktdrhbz",
+  "wlpiogovyfxcjuqmnxakkdrhbz",
+  "wlpiogsvyfecjkqmdxaktmrhbz",
+  "wlpiogsvyfecjwqmntaktdhhbz",
+  "wlpiogsvdfecjuqmmxaktbrhbz",
+  "wlpiogsvyfecauqmnxaksdrhwz",
+  "wlpiogsvwfecjuqznxaktorhbz",
+  "wlpiogtvyfecjuqhnxakidrhbz",
+  "wlpiogsvyyecjuqmnxaktdrhwt",
+  "wljiogsvyfecfuqbnxaktdrhbz",
+  "wlpiogsvybecjuqmnxagtdrjbz",
+  "wrpiogsvyfecjuqmnuaktdrhbd",
+  "wlpiogsvyfecjurmnxnltdrhbz",
+  "blpvogsvyaecjuqmnxaktdrhbz",
+  "bfpiogyvyfecjuqmnxaktdrhbz",
+  "wlpiogsvyfecjuqinxaknddhbz",
+  "wlpizgsvvfecjuqxnxaktdrhbz",
+  "glpiogsvyrecjuqmnxaktdrhbr",
+  "wlpiogskhfecjutmnxaktdrhbz",
+  "wlpiogsvyfecmuqmnxaktdribc",
+  "wlpioesvwfecjuqmnxakkdrhbz",
+  "wlpionsrafecjuqmnxaktdrhbz",
+  "wlsiogsvyfecjuqmnaaktdrhvz",
+  "bloiogsvyfecjuqmnxakjdrhbz",
+  "wlpiogsvyfecjuqenmastdrhbz",
+  "wlpiogyvyfecjuqmuxakldrhbz",
+  "plpiogovyfecjuvmnxaktdrhbz",
+  "wlpiogsvyfebjuqmnkakvdrhbz",
+  "wlziogsvyfhcjuqmngaktdrhbz",
+  "wlsiogsvyfecjuqmnxaktdrzjz",
+  "plbiogsvyfecfuqmnxaktdrhbz",
+  "wfpiogsvyfecjuqknxaktdrhiz",
+  "wlpiogjbyfecjuqmnxaktprhbz",
+  "wmpiogsvyrecjcqmnxaktdrhbz",
+  "wlpiogsyyfecjuqmqxaktdrbbz",
+  "wlpiogsvyfecjuqknxaetdehbz",
+  "wlpiogsvyfezjuqmnxakkdhhbz",
+  "wlpiogsvyfecjjqvnxaktdrhiz",
+  "wkpiogsvyfucjuqmnxaktdrhbd",
+  "lliiogsvyfecjuqmnxaktdrhoz",
+  "wlpiogsvyfecjuqmsxdktdshbz",
+  "wlprogtvyfecjuqmnxaktvrhbz",
+  "wlpizgssyffcjuqmnxaktdrhbz",
+  "wlpioasvyfvcjuqmnxakldrhbz",
+  "wlpoogsvyyecjuqmnxastdrhbz",
+  "wlpiognvyfecjuqmnsaktdrhbr",
+  "wlpiogsoyfecjuqmnxaktdrhho",
+  "wfpiogsvydecjuqmnxaotdrhbz",
+  "wlpiogsvqhecjuqmnxaktdrhhz",
+  "wkpiogsvyfeojuqmnxaktdrqbz",
+  "wlpiogsvyfeveuqmnxaktdshbz",
+  "wlpiogbvyfecjuqmexaktdrcbz",
+  "wlpxogsvyfehjsqmnxaktdrhbz",
+  "wlpcogsvyfecjuqmnjakttrhbz",
+  "wlpiogsvvkecjuqmnxaftdrhbz",
+  "wlpiogsvffecnuqmnxaktdnhbz",
+  "wlpiogsvyfecjupjnxaktdrhbr",
+  "wlpqogsvyfecjuqmnxlktdphbz",
+  "wlpxogsvyfecjvqmnxaktirhbz",
+  "elpiogsvyfecjuqlnxaqtdrhbz",
+  "wspiogsvrfecjuqmnxakadrhbz",
+  "wlpiogsmyfecbuqmnxactdrhbz",
+  "wlpiogsvyfecauqmnyakzdrhbz",
+  "wlsiogsvyfecjuqmnxakvdrnbz",
+  "wlpiogsxyfeijuqmnxakndrhbz",
+  "wlpiogsvyfecjuumnxakbyrhbz",
+  "wlpiogsvyfecjuqmnxhktdyhbo",
+  "wlpiogsvyfecjuqqnxnjtdrhbz",
+  "wapiogsvyfecjuqmnxoktdrmbz",
+  "wlpiogsvyfeejvqmnxaktdrubz",
+  "wlpitgsvyeectuqmnxaktdrhbz",
+  "alpiogsvyfecjulmnxaktdchbz",
+  "wlpiogsvyfecjuqmuxoktdrwbz",
+  "wlpiogsvyfzgjuhmnxaktdrhbz",
+  "wlpnogsvyfecjuqmdxaktyrhbz",
+  "wlpiogsvyfecjuqmnxakthrhra",
+  "wliiogsvyfecluqmnxaktdhhbz",
+  "wlpiogsvyfecjuymnxaltdrhwz",
+  "wlpiogsvyfeljuqmnxaktyrhbd",
+  "wlpiygsvvfecjuqmfxaktdrhbz",
+  "wlpiogihsfecjuqmnxaktdrhbz",
+  "wlpiogjvyfecjuqmnhuktdrhbz",
+  "wldiogsvyfecjiqmwxaktdrhbz",
+  "wlpiogsvjfecjuqmnxaktdrgbr",
+  "wlpioisvyfecjuqwnxabtdrhbz",
+  "wlviogsvyfscjuqmnxqktdrhbz",
+  "wlpiogsvyfecjuqmuxakbdrubz",
+  "wlpiogsvyfecjuqmnxmatdrhqz",
+  "wlpiogsvyfbcjuqwmxaktdrhbz",
+  "wlpiogsvyfexjuqmnxuxtdrhbz",
+  "wljiogsvbfecjuqmnxartdrhbz",
+  "wlpvogsvyfeujuqmnxaktdmhbz",
+  "wnpiogsvyfekjuqanxaktdrhbz",
+  "wlprogsvyfecjuqmzxvktdrhbz",
+  "wkpiogvvyfecjuqmnxaktdrabz",
+  "wlpiogsvwfecjuqmnxaktkbhbz",
+  "wlpiogsvyfecjlqmnxtttdrhbz",
+  "wlpioqsvyfecjuqznxaktyrhbz",
+  "wlpiogsvyfecjuqmnxnethrhbz",
+  "wlpiogsyyfgcjuqmnxaktdrhbm",
+  "wlpiopsvbfecjuqmnxaktdlhbz",
+  "wloqogsvyfucjuqmnxaktdrhbz",
+  "wlpiogsvmfecjuqmnxmktdrhtz",
+  "wlhiogsvyfecjuhmnxaktsrhbz",
+  "wlpioggvpfecjufmnxaktdrhbz",
+  "wlpiogsvyfbcjuomnxaktdrhbh",
+  "wlpmogsvyfecyuqmnxoktdrhbz",
+  "wlpiogslyfecjuqmnxaptyrhbz",
+  "tlpiogsnyfecguqmnxaktdrhbz",
+  "wlpiogsvyfecjuqmnxwktwehbz",
+  "wlpiogsvgfecjuqmnxaktdrabu",
+  "wbpiogsvyfecjuqmnxagtdrhoz",
+  "wlwipgsvyfecjuqmnxaktdrhbu",
+  "wlpwogsvykeczuqmnxaktdrhbz",
+  "wlpgogsvwfecjuqmnxcktdrhbz",
+  "wlpiogsqyfecjuqmrxaktdrrbz",
+  "wlpiogsvyfecjuqmnxakthrafz",
+  "wypicgseyfecjuqmnxaktdrhbz",
+  "wlpiogcvqfecjuqmnxaktdrhzz",
+  "wlriogsvyfecouqmnkaktdrhbz",
+  "wlpiogsvyfemjulmnxaktdrhdz",
+  "flpiogadyfecjuqmnxaktdrhbz",
+  "wupiogsvyfbvjuqmnxaktdrhbz",
+  "wlpiogsvyfebjummnxaktdrrbz",
+  "wjpiogsvyfecjuqmnxaktprybz",
+  "wlpirgsvyfecjiqmnxaatdrhbz",
+  "bvpiogsvyfecjuqmnxaktdrhez",
+  "wlpiogsvyfxcjuqmnxykzdrhbz",
+  "wlkiwgsqyfecjqqmnxaktdrhbz",
+  "wepaogsvyfecjxqmnxaktdrhbz",
+  "wlpiovsvyfecjjqmnxaktdmhbz",
+  "wlpioysryfecjuqmnxaktdrhiz",
+  "wlpizjsvyfecjuvmnxaktdrhbz",
+  "dlpiogsvyfecjucmnxakbdrhbz",
+  "wlpiogsccfecjrqmnxaktdrhbz",
+  "wlpioggvyfecpuqmnxagtdrhbz",
+  "wlpiogsvyfvcjuumlxaktdrhbz",
+  "wwpiogsryfjcjuqmnxaktdrhbz",
+  "wlpiogsvyfecjuqynxaktdrogz",
+  "wlpiogsvyfecjpqmnxskbdrhbz",
+  "wlpiogsvyfecjuhmfxaktvrhbz",
+  "wlpiogevyfecjrqmnwaktdrhbz",
+  "wlpiigsvyfemjuqmnxaktdrhtz",
+  "wlpcogsvyfecjuqhnxakgdrhbz",
+  "wupiogsvyfxcjuqmnxaktdrhgz",
+  "wlsiogsvyfecjuqenxuktdrhbz",
+  "wlpioglvyfecjujmexaktdrhbz",
+  "wlriogsvyfeljuqmnxattdrhbz",
+  "wlpiogsvyfecfuqmhxaktkrhbz",
+  "wlppogsvyfecjuqmxxabtdrhbz",
+  "wlniogsvyfevjuqwnxaktdrhbz",
+  "wlhiogsvyfecjuqmnxactxrhbz",
+  "ilpiogivyflcjuqmnxaktdrhbz",
+  "wlpmogsvyfecjuqmnxaktdrlbs",
+  "wipiogsvyfeqjuqmnxaktrrhbz",
+  "wvpiogsvyfecjuqknxaktdrrbz",
+  "wwpioguvyfecxuqmnxaktdrhbz",
+  "wlpiogsvkfdcjuqmnxaktdzhbz",
+  "wlpiogfvyfecjuqmnxadtdrhbg",
+  "wlpiogsvyzefjuqfnxaktdrhbz",
+  "wlpiogstyfechuqmnxaktdchbz",
+  "wlpiogszyfedjuqmnxsktdrhbz",
+  "wzpiozsvyfncjuqmnxaktdrhbz",
+  "xlpiogsvyfefjuqmnmaktdrhbz",
+  "wlpiogsvyfebxummnxaktdrhbz",
+  "wlpiogsgyfecfurmnxaktdrhbz",
+  "wlpqogsvyfecjuomnxaktdrhbi",
+  "wlpiogjvufecjuqmnxaktdrhbd",
+  "wlpiolsvyfecduqmnxaktrrhbz",
+  "wlpxogsvyfecjuqmnxaktgrhbk",
+  "wlpiogsfyfncjuqmnxsktdrhbz",
+  "wlpioggvyfecjufmnxaktdrebz",
+  "wlpiogsvyfecfujmnxaktdrwbz",
+  "rlpiogsvyfecjlqmnxaktdqhbz",
+  "wlpfogsvyfecjuimnxaktfrhbz"
+];
+
+// console.log(inventory);
+
+// //a place to store the 2counts and a place to store the 3 counts
+// // turn each index into an object of counts of letters
+// // iterate through the objects and incriment the 2 count and the 3 count accordingly
+// // return the product of 2count with 3count
+// const inventoryCheckSum = inventoryArr => {
+//   let count2s = 0,
+//     count3s = 0,
+//     objArr = [];
+
+//   for (let i = 0; i < inventoryArr.length; i++) {
+//     let str = inventoryArr[i];
+//     let counts = {};
+//     let ch, index, len, count;
+//     for (index = 0, len = str.length; index < len; ++index) {
+//       ch = str.charAt(index);
+//       count = counts[ch];
+//       counts[ch] = count ? count + 1 : 1;
+//     }
+//     objArr.push(counts);
+//   }
+//   // console.log(objArr);
+//   console.log(count2s);
+//   console.log(count3s);
+
+//   objArr.forEach(
+//     obj => {
+//       // for(key in obj) {
+
+//       if (Object.values(obj).includes(2)) {
+//         count2s++;
+//       }
+//       if (Object.values(obj).includes(3)) {
+//         count3s++;
+//       }
+//     }
+
+//     // })
+//   );
+//   return count2s * count3s;
+// };
+
+// inventoryCheckSum(inventory);
+
+// --- Day 2: Inventory Management System ---
+// You stop falling through time, catch your breath, and check the screen on the device. "Destination reached. Current Year: 1518. Current Location: North Pole Utility Closet 83N10." You made it! Now, to find those anomalies.
+
+// Outside the utility closet, you hear footsteps and a voice. "...I'm not sure either. But now that so many people have chimneys, maybe he could sneak in that way?" Another voice responds, "Actually, we've been working on a new kind of suit that would let him fit through tight spaces like that. But, I heard that a few days ago, they lost the prototype fabric, the design plans, everything! Nobody on the team can even seem to remember important details of the project!"
+
+// "Wouldn't they have had enough fabric to fill several boxes in the warehouse? They'd be stored together, so the box IDs should be similar. Too bad it would take forever to search the warehouse for two similar box IDs..." They walk too far away to hear any more.
+
+// Late at night, you sneak to the warehouse - who knows what kinds of paradoxes you could cause if you were discovered - and use your fancy wrist device to quickly scan every box and produce a list of the likely candidates (your puzzle input).
+
+// To make sure you didn't miss any, you scan the likely candidate boxes again, counting the number that have an ID containing exactly two of any letter and then separately counting those with exactly three of any letter. You can multiply those two counts together to get a rudimentary checksum and compare it to what your device predicts.
+
+// For example, if you see the following box IDs:
+
+// abcdef contains no letters that appear exactly two or three times.
+// bababc contains two a and three b, so it counts for both.
+// abbcde contains two b, but no letter appears exactly three times.
+// abcccd contains three c, but no letter appears exactly two times.
+// aabcdd contains two a and two d, but it only counts once.
+// abcdee contains two e.
+// ababab contains three a and three b, but it only counts once.
+// Of these box IDs, four of them contain a letter which appears exactly twice, and three of them contain a letter which appears exactly three times. Multiplying these together produces a checksum of 4 * 3 = 12.
+
+// What is the checksum for your list of box IDs?
